7cc7a9f3f9840b9075e40e272abdc66b83d2e3d1
tu.vasilev <taras.vasiliev@gmail.com>
2021-01-09 16:12:47 +0500

    JS features

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -10,10 +10,10 @@ var search = new Vue({
 var form = new Vue({
     el: "#search-form",
     methods: {
-        async getSearch: function (e) {
+        getSearch: function (e) {
             const cards = this.$refs.cards.value;
             console.log("beginning of post")
-            const response = await axios.post("/search", cards);
+            const response = axios.post("/search", cards);
             console.log("end of post")
             search.searchResult = response;
         }
