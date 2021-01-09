ea9972187dfd573bad5bb318e5623d33eb6e0a03
tu.vasilev <taras.vasiliev@gmail.com>
2021-01-09 19:22:54 +0500

    Form validation

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -10,19 +10,31 @@ var search = new Vue({
 var form = new Vue({
     el: "#search-form",
     data: {
+            errors: [],
             cards: "",
             allowEmpty: "",
             allowArt: ""
     },
     methods: {
+        checkForm: function (e) {
+            if (!this.cards) {
+                errors.push("List of cards cannot be empty")
+            }
+        },
         getSearch: function (e) {
+            this.errors = [];
+            this.checkForm();
+            
+            if (this.errors.length)
+                return;
+
             const response = axios
                 .post(
                     "/search", 
                     {
-                        cards: this.$refs.cards.value.split(/\r?\n/),
-                        allow_empty: this.allowEmpty,
-                        allow_art: this.allowArt
+                        cards: this.cards.split(/\r?\n/),
+                        allow_empty: this.allowEmpty || false,
+                        allow_art: this.allowArt || false
                     }
                 )
                 .then(response => (search.searchResult = response.data));
